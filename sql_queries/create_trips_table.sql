63eb8b03a5e89d9366f2bb0b29d571e9bb1d0f36
ZeroSum24 <stephen_waddell@hotmail.co.uk>
2018-11-30 14:52:32 +0000

    Latest progess on sql queries

---
+++ sql_queries/create_trips_table.sql
@@ -0,0 +1,25 @@
+-- Creating parent table to organise hold the trips together, have them inherit with a consistent schema
+
+-- creating the parent table
+CREATE TABLE trips_table (index bigint, name text, email text, position text, owes double precision, paid text, payment_method text ) ;
+
+
+-- altering the tables to have the same schema as the parent
+
+ALTER TABLE cairngorm SET SCHEMA (index bigint, name text, email text, position text, owes double precision, paid text, payment_method text ) ;
+ALTER TABLE crianlarich SET SCHEMA (index bigint, name text, email text, position text, owes double precision, paid text, payment_method text ) ;
+ALTER TABLE lakes_trip SET SCHEMA (index bigint, name text, email text, position text, owes double precision, paid text, payment_method text ) ;
+ALTER TABLE loch_lochy SET SCHEMA (index bigint, name text, email text, position text, owes double precision, paid text, payment_method text ) ;
+ALTER TABLE nov_sat_day_trip SET SCHEMA (index bigint, name text, email text, position text, owes double precision, paid text, payment_method text ) ;
+ALTER TABLE nov_sun_day_trip SET SCHEMA (index bigint, name text, email text, position text, owes double precision, paid text, payment_method text ) ;
+ALTER TABLE ratagan SET SCHEMA (index bigint, name text, email text, position text, owes double precision, paid text, payment_method text ) ;
+
+
+-- altering the tables to inherit the parent
+ALTER TABLE cairngorm INHERIT trips_table ;
+ALTER TABLE crianlarich INHERIT trips_table ;
+ALTER TABLE lakes_trip INHERIT trips_table ;
+ALTER TABLE loch_lochy INHERIT trips_table ;
+ALTER TABLE nov_sat_day_trip INHERIT trips_table ;
+ALTER TABLE nov_sun_day_trip INHERIT trips_table ;
+ALTER TABLE ratagan INHERIT trips_table ;
--- sql_queries/debt_list.sql
+++ sql_queries/debt_list.sql
@@ -1,8 +1,11 @@
 
 -- https://stackoverflow.com/questions/14931001/select-from-all-tables-mysql
+-- https://www.codeproject.com/Questions/385131/run-same-command-for-all-tables-in-a-databse-of-sq
 
-select DISTINCT(tbl."Name"), tbl."Email", tbl."Owes"
-from (SELECT table_name
-      FROM information_schema.tables
-      WHERE table_schema='public') tbl
-where tbl."Paid" IS NULL ;
+-- Select DISTINCT(name), email, owes
+-- from (SELECT table_name
+--       FROM information_schema.tables
+--       WHERE table_schema='public') tbl
+-- where paid IS NULL ;
+
+-- Create parent table and schema get all trips to inherit from this 
